769d7dc8766e10956e1957013b6fdcc557d34e0f
Robert Bocchino <bocchino@me.com>
2012-08-01 19:37:14 -0400

    Suppress debug output

--- src/sle/plugin/scala/tools/sle/ResolveNames.scala
+++ src/sle/plugin/scala/tools/sle/ResolveNames.scala
@@ -2,6 +2,7 @@ package scala.tools.sle
 
 import scala.tools.nsc._
 import scala.tools.nsc.plugins.PluginComponent
+import scala.reflect.internal._
 
 /** This class implements a plugin component using a tree
  *  traverser */
@@ -9,11 +10,16 @@ class ResolveNames (val global: Global) extends PluginComponent {
   import global._
   import global.definitions._
 
+  val debug = false
+
+  def report(a:Any) = {
+    if (debug) println(a)
+  }
+
   val runsAfter = List[String]("refchecks")
   /** The phase name of the compiler plugin
-   *  @todo Adapt to specific plugin.
    */
-  val phaseName = "slecResolveNames"
+  val phaseName = "sleResolveNames"
 
   def newPhase(prev: Phase): Phase = new TraverserPhase(prev)
   class TraverserPhase(prev: Phase) extends StdPhase(prev) {
@@ -26,9 +32,11 @@ class ResolveNames (val global: Global) extends PluginComponent {
 
   def check(tree: Tree): Unit = tree match {
     case Apply(fun, args) =>
-      println("traversing application of " + fun)
-    case ClassDef(mods, name, tparams, impl) =>
-      println("traversing class def " + name)
+      report("traversing application of " + fun)
+    case ClassDef(mods, name, tparams, impl) => {
+      report("traversing class def " + name)
+      report(tree.symbol.getAnnotations)
+    }
     case _ => ()
   }
 }
